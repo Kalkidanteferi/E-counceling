83c6fd56d837b237e9e5894f1dade8eca66d6aa2
Kalkidanteferi <39376098+Kalkidanteferi@users.noreply.github.com>
2019-12-20 02:59:34 -0800

    Add files via upload

--- views.py
+++ views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
 
 # Create your views here.
+
+
+def Contact(request):
+    return render (request, 'Contact/Contact.html')
